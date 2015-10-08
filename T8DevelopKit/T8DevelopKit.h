6d36933b5d6946b3e6e030dc33b7983da62f26d9
zhangqi <zhangqi@t8.io>
2015-10-08 20:35:39 +0800

    update .h file for module

--- T8DevelopKit/T8DevelopKit.h
+++ T8DevelopKit/T8DevelopKit.h
@@ -19,4 +19,39 @@
 #import "UIImage+T8.h"
 #import "UIView+T8.h"
 
+#import "T8BaseTableViewCell.h"
+#import "T8InsetsTextField.h"
+#import "T8BaseNavigationController.h"
+#import "T8TableViewController.h"
+#import "T8MenuTableViewController.h"
+
+#import "SVPullToRefresh.h"
+#import "T8PlaceHolderTextView.h"
+
+#import "T8MenuItem.h"
+#import "T8MenuTitleItem.h"
+#import "T8MenuTitleCustomViewItem.h"
+#import "T8MenuTextFieldItem.h"
+#import "T8MenuTextViewItem.h"
+#import "T8MenuDescriptionItem.h"
+#import "T8MenuFunctionItem.h"
+#import "T8MenuSwitchItem.h"
+#import "T8MenuSection.h"
+
+#import "T8JsonHelper.h"
+#import "T8SandBoxHelper.h"
+#import "T8CameraHelper.h"
+#import "T8UserDefaultHelper.h"
+
+#import "T8MenuCell.h"
+#import "T8MenuTitleCell.h"
+#import "T8MenuTitleCustomViewCell.h"
+#import "T8MenuTextFieldCell.h"
+#import "T8MenuTextViewCell.h"
+#import "T8MenuDescriptionCell.h"
+#import "T8MenuFunctionCell.h"
+#import "T8MenuSwitchCell.h"
+#import "T8ATBCell.h"
+#import "T8AvatarAndTitleCell.h"
+
 #endif
--- T8DevelopKit/T8MenuTableViewController/Cells/T8MenuCell.h
+++ T8DevelopKit/T8MenuTableViewController/Cells/T8MenuCell.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-#import <Masonry.h>
+#import "Masonry.h"
 #import "T8Defines.h"
 
 @interface T8MenuCell : UITableViewCell
